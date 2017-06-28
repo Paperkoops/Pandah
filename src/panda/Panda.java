21da4dfabd72196963448e15b7387065b3770d2e
Paperkoops <Paperkoops@192.168.1.12>
2017-06-28 10:38:00 -0600

    New Comit Chido x2

---
+++ src/panda/Panda.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package panda;
+
+/**
+ *
+ * @author Paperkoops
+ */
+public class Panda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static panda.JFrames.Principal mamain = new panda.JFrames.Principal();
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //panda.JFrames.Principal gltt = new panda.JFrames.Principal();
+       mamain.setVisible(true);
+    }
+    
+}
